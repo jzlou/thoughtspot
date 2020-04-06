a5462c7f98c1ec01feafb08ffc4f88b150d3b34c
Jeff Lou <jeffzlou@gmail.com>
2020-04-05 21:37:58 -0400

    two pages!

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+export default function About() {
+  return (
+    <div>
+      <p>most people are other people. their thoughts are someone else's opinion, their lives a mimicry, their passions a quotation</p>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export default function Index() {
   return (
     <div>
       <p>i am to wait, though waiting be so hell</p>
+      <Link href="/about">
+        <a>about</a>
+      </Link>
     </div>
   )
 }
